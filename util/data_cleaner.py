fa592acf9fa6057da9026055e3e7f5c957d2fbdb
Nick Tehrany <nicktehrany1@gmail.com>
2020-02-15 19:00:10 +0100

    Improved regex substitution
    Also moved complex features into a function and removed useless variables

--- util/data_cleaner.py
+++ util/data_cleaner.py
@@ -9,6 +9,34 @@ import pandas as pd
 from nltk.corpus import words
 from progress.bar import Bar
 nltk.download('words')
+
+def clean_row(row):
+
+    # Removes The unused text inside [], (), and {} like [Verse 1:] etc..
+    row[6] = re.sub(r'\[\[*[^]]*\]\]*|\(.[^)]*\)\)*|\{\{*[^}]*\}\}*', '', row[6])
+    split = row[6].split()
+
+    # Only writes Songs with English Lyrics to new dataset (Doesn't recognize some words
+    # in Rap songs therefore testing multiple random words to hopefully recognize one)
+    # Encoding since csv.read returns a string and we need bytes in order to check
+    # if it's some character besides ascii by decoding it 
+    try:
+        if (len(split) > 1):
+            str.encode(split[1]).decode('ascii')
+    except UnicodeDecodeError:
+        pass
+    else:
+        # If Lyrics are less than 6 words the row is also not written to the file (TODO: Change to a more appropiate number. I chose 6 randomly)
+        if (len(split) > 6):
+            try:
+                str.encode(split[6]).decode('ascii')
+            except UnicodeDecodeError:
+                pass
+            else:
+                if (split[1] in words.words() or split[6] in words.words()):
+                    csv_writer.writerow([row[0], row[2], row[5], row[6], row[7]])
+
+                        
 with open('songs_dataset.csv', 'r') as dataset, open('data/dataset.csv', 'w+') as out:
     csv_writer = csv.writer(out)
     header = True
@@ -17,38 +45,11 @@ with open('songs_dataset.csv', 'r') as dataset, open('data/dataset.csv', 'w+') a
     pd_reader = pd.read_csv('songs_dataset.csv')
     bar = Bar('Cleaning', max = len(pd_reader) + 1)
     for row in csv.reader(dataset):
-        bar.next()
         if not header:
-            text = row[6]
-
-            # Removes The unused text with [], (), and {} like [Verse 1:] etc..
-            row[6] = re.sub(r'\[\[*[a-zA-Z ]*\]\]*|\(.[a-zA-Z ]*\)\)*|\{\{*[a-zA-Z ]*\}\}*', '', text)
-            text = row[6]
-            split = text.split()
-
-            # Only writes Songs with English Lyrics to new dataset (Doesn't recognize some words
-            # in Rap songs therefore testing multiple random words to hopefully recognize one)
-            # Encoding since csv.read returns a string and we need bytes in order to check
-            # if it's some character besides ascii by decoding it 
-            try:
-                if (len(split) > 1):
-                    str.encode(split[1]).decode('ascii')
-            except UnicodeDecodeError:
-                pass
-            else:
-
-                # If Lyrics are less than 6 words the row is also not written to the file (TODO: Change to a more appropiate number. I chose 6 randomly)
-                if (len(split) > 6):
-                    try:
-                        str.encode(split[6]).decode('ascii')
-                    except UnicodeDecodeError:
-                        pass
-                    else:
-                        if (split[1] in words.words() or split[6] in words.words()):
-                            csv_writer.writerow([row[0], row[2], row[5], row[6], row[7]])
-
+            clean_row(row)
+        bar.next()
         header = False
     
     bar.finish()
     dataset.close()
-    out.close()           
+    out.close()
