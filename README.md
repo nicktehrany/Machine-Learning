8e6b52d6a7ee2e5974f436a17c34cbfc8339db05
Nick Tehrany <nicktehrany1@gmail.com>
2020-03-02 21:36:52 +0100

    Updated Neural networks to use helper functions

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# DATASET CLEANUP
+# DATASET CLEANUP AND DATA SPLIT
 ## Required Packages
+
+**Test and Train data is included in the repo**
+
 Download the songs_dataset.csv from https://www.kaggle.com/detkov/lyrics-dataset/version/5#songs_dataset.csv and store it in your project's home directory then run the
 following commands to install needed libraries and create a new clean dataset:
 
@@ -11,6 +14,14 @@ make clean-dataset
 ```
 
 This will take a few hours, therefore ran the script once and included the dataset in the src folder.
+The dataset cleaner script is in the util directory and will display several progress bars on the terminal when running.
+
+After running the cleanuo script we ran the data split once to create the test and train data sets, which are both stored in the src folder.
 
 # Models
 All models are in the src folder. (TODO: Describe different Models)
+
+# Helper Functions
+To keep the code for the models simple and compact, we placed all our functions (cleaning text, creating one-hot vectors, etc.)
+inside a file called helper functions in the util directory. We import this file on begin of the model script and call the functions as
+needed.
--- util/helperfunctions.py
+++ util/helperfunctions.py
@@ -1,4 +1,12 @@
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categorical
+import re # for removing special characters
+from nltk.stem import WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.corpus import stopwords # For removing stopwords
 import numpy as np
 
 # creates a one-hot zero initialized vector of size size
@@ -18,3 +26,58 @@ def get_index(word, word_list):
         if word == value:
             return index
     return -1
+
+
+# takes a list and creates a one-hot vector for the genre
+def one_hot_genres(genres):
+    one_hot = []
+
+    for value in genres:
+        index = 0
+        if value == '[\'Pop\']': index = 0
+        elif value == '[\'Rock\']': index = 1
+        elif value == '[\'Hip-Hop/Rap\']': index = 2
+        elif value == '[\'Country\']': index = 3
+        elif value == '[\'R&B/Soul\']': index = 4
+        elif value == '[\'Metal\']': index = 5
+        elif value == '[\'Alternative/Indie\']': index = 6
+        elif value == '[\'Folk\']': index = 7
+        one_hot.append(index)
+
+    one_hot = to_categorical(one_hot, 8)
+    return one_hot
+
+# takes a list and a boolean. cleans the text in the list and if boolean is true,
+# creates a list of all words that occured in the text. returns cleaned text and
+# and list of words or empty list
+def clean_text(text_list, use_list):
+    corpus = []
+    sw = stopwords.words("english")
+    word_list = []
+    for text in text_list:
+        text = re.sub('[^a-zA-Z]', ' ', text) # removes special characters
+        text = text.lower() # lowercases everything
+        text = text.split() # splits words
+        text = [wordnet_lemmatizer.lemmatize(word, pos="v") for word in text if not word in set(sw)]
+        formatted_text = ""
+        for word in text:
+            if use_list:
+                if word not in word_list:
+                    word_list.append(word)
+            formatted_text+=word+" "
+        corpus.append(formatted_text)
+    text_list = corpus
+
+    return (text_list, word_list)
+
+# reverses the one-hot genres vector back to genres and returns it
+def one_hot_reverse(genre_list):
+    genres = []
+
+    for i in range(len(genre_list)):
+        x = genre_list[i]
+        for index in range(0, 8):
+            if x[index] == 1.0:
+                genres.append(index)
+
+    return genres
